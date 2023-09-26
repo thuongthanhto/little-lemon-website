2c72ea146f0fb7f28912b298d1c89838ee1073db
Thuong To <thuongthanhto@gmail.com>
2023-09-26 14:03:14 +0700

    Done

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import About from "./About";
+import HeroSection from "./HeroSection";
+import "./Main.scss";
+import SpecialsMenu from "./SpecialsMenu";
+import Testimonials from "./Testimonials";
+// import Menu from "./Menu";
+const Main = () => {
+  return (
+    <main>
+      <HeroSection />
+      <SpecialsMenu />
+      <Testimonials />
+      <About />
+    </main>
+  );
+};
+
+export default Main;
